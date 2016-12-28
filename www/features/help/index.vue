59b134f9050fa4303b691eab76aef4a481f3b9a0
Howard.Zuo <leftstick@qq.com>
2016-12-28 14:09:38 +0800

    update help with fallback introduction

--- www/features/help/index.vue
+++ www/features/help/index.vue
@@ -3,34 +3,69 @@
         <div class="content">
             <h1 class="title">FAQ</h1>
             <div class="block">
-                <h2 class="sub-title">What is naive mock?</h2>
+                <h2 id="what-is-mock" class="sub-title"><a href v-anchor="'what-is-mock'">What is naive mock?</a></h2>
                 <p class="desc"><code>naive mock</code> is a dead simple mock system which aims helping front-end developers build up applications quickly, parallel with API development.</p>
             </div>
             <div class="block">
-                <h2 class="sub-title">How to create a mock API?</h2>
+                <h2 id="how-to-create-mock-api" class="sub-title"><a href v-anchor="'how-to-create-mock-api'">How to create a mock API?</a></h2>
                 <p>Go to <router-link :to="{ path: '/apimanager' }">API Management</router-link> page. Find <code>Actions</code> menu-item at the top-right corner, Click <code>Create API</code> menu. As below figure:</p>
                 <img src="../../img/help/click-menu.png">
                 <p>Type API meta you want to mock, as below figure:</p>
                 <img src="../../img/help/create-api.png">
-                <p>The mock API shall be available once you click <code>Create</code> button.</p><br/>
+                <p>The mock API shall be available once you click <code>Create</code> button.</p>
                 <p>At <router-link :to="{ path: '/apimanager' }">API Management</router-link> page, you may find newly created API, click the <code>API</code> column, verify command will be displayed for you. </p>
                 <img src="../../img/help/api-verify.png">
                 <blockquote>
                     Note: the <code>header</code> of <code>category</code> is mandatory, otherwise, you will get <code>404</code> while using this mock API.
                 </blockquote>
             </div>
+            <div class="block">
+                <h2 id="what-is-fallback" class="sub-title"><a href v-anchor="'what-is-fallback'">What is fallback?</a></h2>
+                <p><code>fallback</code> is a backup server used when the API is not mocked with <code>naive-mock</code>. This is useful when you use <code>naive-mock</code> as replacement of your real backend server, but you do not want to mock all the server APIs in <code>naive-mock</code>.</p>
+                <p>Go to <router-link :to="{ path: '/settings' }">Settings</router-link> page. You will see <code>fallback</code> config. As below figure:</p>
+                <img src="../../img/help/settings-fallback.png">
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+const scrollTo = function(el) {
+    setTimeout(function() {
+        window.scrollTo(0, el.getBoundingClientRect().top + (window.pageYOffset || document.documentElement.scrollTop));
+    });
+};
 
 export default {
+    mounted() {
+        const hash = this.$route.hash;
+        if (hash) {
+            scrollTo(document.querySelector(hash));
+        }
+    },
+    directives: {
+        anchor: {
+            bind(el, binding, vnode) {
+                el.addEventListener('click', function(e) {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    window.location.hash = binding.value;
+                    scrollTo(el);
+                }, false);
+            }
+        }
+    }
 };
 
 </script>
 
 <style scoped>
+    a {
+        text-decoration: none;
+    }
+    p {
+        margin: 5px 0;
+    }
     .help {
         width: 100%;
         height: 100%;
@@ -64,12 +99,14 @@ export default {
 
     .title,
     .sub-title {
-        color: #0271a8;
         margin-bottom: 15px;
+        a {
+            color: #0271a8;
+        }
     }
 
     .block {
-        margin-bottom: 15px;
+        margin-bottom: 30px;
     }
 
     img {
